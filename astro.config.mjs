bc33c9c3c94d64f7ff9d053bb4e6b86c6256784c
Robert Fisher <fisher.robert26@gmail.com>
2026-03-27 15:51:47 -0500

    Initial 'pond' feature for displaying pipeline visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   site: 'https://guppyfisher.dev',
   markdown: {
     shikiConfig: {
